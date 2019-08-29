9e84e304251ac5cd7a1e6e6d3310d1609ed690d2
Ivan Tsai <oscar60310@gmail.com>
2019-08-29 15:52:46 +0800

    install koa framework

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-import chalk from 'chalk';
-console.log(chalk`{green ts env work}`);
+import * as koa from 'koa';
+
+const app = new koa();
+const port = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log(`[Web Server] Server start at ${port}`);
+});
